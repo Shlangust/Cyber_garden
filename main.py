cc993db4970c161811b28fbd0b17fe8de3e4b427
Vanya351 <goloborodko2005@gmail.com>
2024-06-09 00:39:33 +0300

    авто старт дрона

--- main.py
+++ main.py
@@ -5,12 +5,5 @@ from drone_control import Drone
 
 
 if __name__ == "__main__":
-    drone1 = Drone('localhost', '5762')
-
-    drone1.enter_guided_mode()
-    time.sleep(1)
-    drone1.arm_drone()
-    time.sleep(1)
-    drone1.take_off()
-    time.sleep(4)
-    drone1.go_to_local_position(20, 10, 10)
+    drone = Drone('localhost', '5762')
+    drone.auto_start(10)
