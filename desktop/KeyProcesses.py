7a2e1888dcc09e8c3cda8bd4d16c9115da59236f
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-06-08 12:55:16 +0300

    обработка нажатия клавиш

---
+++ desktop/KeyProcesses.py
@@ -0,0 +1,51 @@
+
+def press_w():
+    pass
+
+
+def press_a():
+    pass
+
+
+def press_s():
+    pass
+
+
+def press_d():
+    pass
+
+
+def press_q():
+    pass
+
+
+def press_e():
+    pass
+
+
+def press_shift():
+    pass
+
+
+def press_enter():
+    pass
+
+
+def press_space():
+    pass
+
+
+def press_up_arrow():
+    pass
+
+
+def press_down_arrow():
+    pass
+
+
+def press_left_arrow():
+    pass
+
+
+def press_right_arrow():
+    pass
